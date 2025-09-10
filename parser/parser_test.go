0379a7561df5c6548632223a0ce2080134081656
DanijelGb <124356150+DanijelGb@users.noreply.github.com>
2025-09-10 10:47:07 +0200

    Update parser_test.go

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -651,43 +651,38 @@ func TestIdentifierExpression(t *testing.T) {
     }
 }
 
-func TestLetStatements(t *testing.T) { 
-    input := `
-    let x = 5; 
-    let y = 10; 
-    let foobar = 838383;` 
+func TestLetStatements(t *testing.T) {
+    tests := []struct {
+        input string
+        expectedIdentifier string
+        expectedValue interface{}
+    }{
+        {"let x = 5;", "x", 5},
+        {"let y = true;", "y", true},
+        {"let foobar = y;", "foobar", "y"},
+    }
 
-    l := lexer.New(input) 
-    p := New(l) 
+    for _, tt := range tests {
+        l := lexer.New(tt.input)
+        p := New(l)
+        program := p.ParseProgram()
+        checkParserErrors(t, p)
 
-    program := p.ParseProgram() 
-    checkParserErrors(t, p)
+        if len(program.Statements) != 1 {
+            t.Fatalf("program.Statements does not contain 1 statements. got=%d",
+                len(program.Statements))
+        }
 
-    if program == nil {
-        t.Fatalf(" ParseProgram() returned nil")
-    } 
-
-    if len( program.Statements) != 3 {
-        t.Fatalf(" program.Statements does not contain 3 statements. got =% d",
-            len( program.Statements)) 
-    } 
-
-    tests := [] struct {
-        expectedIdentifier string 
-    }{ 
-        {"x"},
-        {"y"},
-        {"foobar"}, 
-
-    } 
-
-    for i, tt := range tests {
-        stmt := program.Statements[i]
+        stmt := program.Statements[0]
         if !testLetStatement(t, stmt, tt.expectedIdentifier) {
-            return 
-        } 
-    } 
-}
+            return
+        }
+
+        val := stmt.(*ast.LetStatement).Value
+        if !testLiteralExpression(t, val, tt.expectedValue) {
+            return
+        }
+    }
 
 func TestReturnStatements(t *testing.T) {
     input := `
